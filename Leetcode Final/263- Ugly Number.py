7fdad548bc3f127335aed6a5632917b4daef75e7
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-06-17 16:50:29 +0530

    263- Ugly Number

---
+++ Leetcode Final/263- Ugly Number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isUgly(self, n):
+        if n==0:
+            return False
+        divi=[2,3,5]
+        for i in divi:
+            while n%i==0:
+                n/=i
+
+        return n==1

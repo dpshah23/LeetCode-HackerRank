652cba5b948e8f9bca01c9d9c01b919410c68867
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-06-23 14:08:59 +0530

    Added 945-Minimum Increment to Make Array Unique

---
+++ Leetcode Final/945- Minimum Increment to Make Array Unique.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def minIncrementForUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        numTracker = 0
+        minIncreament = 0
+
+        for num in nums:
+            numTracker = max(numTracker, num)
+            minIncreament += numTracker - num
+            numTracker += 1
+        return minIncreament
+        

aff18f4336b86efa7e4a01abb2e197ef7ebed344
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-12-11 16:27:15 +0530

    Added Leetcode POTD 11 november

---
+++ Leetcode Final/2779 -  Maximum Beauty of an Array After Applying Operation.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maximumBeauty(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums.sort()
+        left = 0
+        max_beauty = 0
+
+        for right in range(len(nums)):
+            while nums[right] - nums[left] > 2 * k:
+                left += 1
+            max_beauty = max(max_beauty, right - left + 1)
+        
+        return max_beauty
+            
